11589362e9c1ee3793ce42e3447504d4ef72b86b
tishulya3377 <mrchisha5@gmail.com>
2026-07-20 06:10:10 +0000

    Codespaces update Mon Jul 20 06:10:10 UTC 2026

--- src/RootShorts.tsx
+++ src/RootShorts.tsx
@@ -5,15 +5,38 @@ import {ShortsVideo} from "./ShortsVideo";
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
-      id="ShortsProject"
-      component={ShortsVideo}
-      fps={30}
-      width={1080}
-      height={1920}
-      durationInFrames={30000}
-      defaultProps={{
-        shortId: 1
-      }}
-    />
+id="ShortsProject"
+component={ShortsVideo}
+fps={FPS}
+width={1080}
+height={1920}
+durationInFrames={1800}
+defaultProps={{
+    shortId:1
+}}
+calculateMetadata={async ({props}) => {
+
+    const shortsPlan = require("../public/shorts_plan.json");
+
+    const short =
+        shortsPlan.shorts.find(
+            (s:any)=>s.id === props.shortId
+        )
+        ||
+        shortsPlan.shorts[0];
+
+
+    const duration =
+        Math.ceil(
+            (short.end - short.start) * FPS
+        );
+
+
+    return {
+        durationInFrames: duration
+    };
+
+}}
+/>
   );
 };
